57666d83bda514834ad56716994be1980448861d
Daria Orlova <da.orlova365@gmail.com>
2024-05-25 17:00:57 +0300

    Initial Commit

--- src/main/java/com/skypro/hw6/service/EmployeeService.java
+++ src/main/java/com/skypro/hw6/service/EmployeeService.java
@@ -4,7 +4,9 @@ import com.skypro.hw6.model.Employee;
 
 public interface EmployeeService {
     Employee add(String firstName, String lastName);
+
     Employee delete(String firstName, String lastName);
+
     Employee find(String firstName, String lastName);
 
 }
